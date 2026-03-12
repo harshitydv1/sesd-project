a7d41ef6ea804770e8b0ab9caa221c34a7ee7479
harshitydv1 <harshit.raj@adypu.edu.in>
2026-03-12 12:00:00 +0530

    feat(backend): setup mongodb and cloudinary configurations

---
+++ backend/config/mongodb.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB");
+  });
+
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+};
+
+export default connectDB;
