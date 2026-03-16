ec7e389555406458daea9b3bb181db28a5154273
harshitydv1 <harshit.raj@adypu.edu.in>
2026-03-16 12:00:00 +0530

    feat(backend): setup user authentication routes

---
+++ backend/routes/userRoute.ts
@@ -0,0 +1,14 @@
+import express, { Router } from "express";
+import {
+  loginUser,
+  registerUser,
+  adminLogin,
+} from "../controllers/userController";
+
+const userRouter: Router = express.Router();
+
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", adminLogin);
+
+export default userRouter;
