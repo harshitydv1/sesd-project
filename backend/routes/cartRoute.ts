5ed2a48105093f12a819cfc5d824b23a3f2c6151
harshitydv1 <harshit.raj@adypu.edu.in>
2026-03-23 12:00:00 +0530

    feat(backend): setup cart management routes

---
+++ backend/routes/cartRoute.ts
@@ -0,0 +1,11 @@
+import express, { Router } from "express";
+import { addToCart, getUserCart, updateCart } from "../controllers/cartController";
+import authUser from "../middleware/auth";
+
+const cartRouter: Router = express.Router();
+
+cartRouter.post("/get", authUser, getUserCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
+
+export default cartRouter;
